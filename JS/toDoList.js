8e1bf032a847d5ec16dea995287f7df7ad9116ca
canansecenmumcu <canansecenmumcu@gmail.com>
2022-03-29 01:05:26 +0300

    first commit

---
+++ JS/toDoList.js
@@ -0,0 +1,48 @@
+let btnDOM = document.querySelector("#liveToastBtn") 
+let listDOM = document.querySelector("#list") 
+let taskDOM = document.querySelector("#task") 
+let ullength = document.getElementsByTagName("li"); 
+
+
+for(let i=0; i < ullength.length;i++){ 
+    let closeButton = document.createElement("span"); 
+    closeButton.textContent = "\u00D7"; 
+    closeButton.classList.add("close"); 
+    closeButton.onclick = removeButton; 
+    ullength[i].append(closeButton); 
+    ullength[i].onclick = check; 
+}
+
+btnDOM.addEventListener('click', elemanEkle)  
+
+function check(){
+  this.classList.toggle("checked"); 
+}
+
+function removeButton(){
+  this.parentElement.remove();  
+}
+
+function elemanEkle() {
+ 
+    if (taskDOM.value.trim() != "" && taskDOM.value.trim() != " "){
+        $(".success").toast("show");
+
+    let liDOM = document.createElement('li') 
+    listDOM.appendChild(liDOM); 
+    liDOM.innerHTML = task.value; 
+    taskDOM.value = "";
+
+    liDOM.onclick = check;    
+    let closeButton = document.createElement("span");
+    closeButton.textContent = "\u00D7";
+    closeButton.classList.add("close");
+    closeButton.onclick = removeButton;
+    liDOM.append(closeButton);
+    listDOM.append(liDOM);
+    inputElement.value = ("");
+
+    }else{  
+    $(".error").toast("show"); 
+} 
+}
